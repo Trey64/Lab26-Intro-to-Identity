3475ad66fd02e1676beeb6e38a8ac492ef18974a
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-18 00:29:32 -0700

    adding identity migration and updated databases

--- Lab26Tom/Startup.cs
+++ Lab26Tom/Startup.cs
@@ -43,6 +43,8 @@ namespace Lab26Tom
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
